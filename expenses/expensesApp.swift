79daa035acfbb22f6fdb9e5c3333156c6b9d8274
Olivera Miatovici <omiatovici@cmedtechnology.com>
2024-07-02 18:39:16 +0300

    Initial Commit

---
+++ expenses/expensesApp.swift
@@ -0,0 +1,32 @@
+//
+//  expensesApp.swift
+//  expenses
+//
+//  Created by Olivera Miatovici on 02.07.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct expensesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
